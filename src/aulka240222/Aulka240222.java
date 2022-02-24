eb0382292d7e25709ab1168a377feb7567620155
João Vittor <joao.vaa03@aluno.ifsc.edu.br>
2022-02-24 17:02:09 -0300

    Inicio do projeto,impreess mi name

---
+++ src/aulka240222/Aulka240222.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulka240222;
+import java .util.Scanner;
+/**
+ *
+ * @author IFSC
+ */
+public class Aulka240222 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner lei=new Scanner(System.in);
+String a;
+System.out.println("Digite seu nome: ");
+a=lei.nextLine();
+System.out.println(a);
+    }
+    
+}
